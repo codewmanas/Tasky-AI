2a069cc5ab6780e90ba99271a3b1a2511bac5b84
codewmanas <manas.kolaskar@somaiya.edu>
2025-03-04 23:11:14 +0530

    Routes setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
+/**
+ * @copyright 2025 Tasky AI
+ *  @license Apache-2.0
+ * @description Main entry point for the Tasky AI application
+ */
+
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+
 import './index.css'
-import App from './App.tsx'
+import router from './routes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
